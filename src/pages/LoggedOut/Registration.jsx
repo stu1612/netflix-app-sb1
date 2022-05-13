6725a2d6dd85779e5397943f8d12dad558e1dac9
Stu Bolderson <stu.bolderson@aol.com>
2022-05-13 12:17:20 +0200

    base css is fine

--- src/pages/LoggedOut/Registration.jsx
+++ src/pages/LoggedOut/Registration.jsx
@@ -1,14 +1,21 @@
 // npm
 import { Link } from "react-router-dom";
+// images
+// import icons from "../../assets/images/devices.png";
 
 export default function Registration() {
   return (
     <section id="registration">
       <div className="registration">
-        <div className="register-icons"></div>
+        <div className="device-icons" />
         <div className="register-header">
-          <span>step 1 of 2</span>
-          <h1>Complete the registration of your account</h1>
+          <p className="step-indicator">
+            step <span className="bolder">1</span> of{" "}
+            <span className="bolder">2</span>
+          </p>
+          <h1 className="registration-title">
+            Complete the registration of your account
+          </h1>
         </div>
         <div className="register-body">
           <p>
@@ -17,7 +24,9 @@ export default function Registration() {
           </p>
         </div>
         <div className="submit-btn-container">
-          <Link to="/signup/regform">Next</Link>
+          <Link to="/signup/regform" className="btn-primary">
+            Next
+          </Link>
         </div>
       </div>
     </section>
